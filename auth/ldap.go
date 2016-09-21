17d7492fd99d9c4196cd61282ba9d8b1cbe24447
Denis Lavrov <bahus.vel@gmail.com>
2016-09-21 16:20:32 +1200

    Added the auth files

---
+++ auth/ldap.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"gopkg.in/ldap.v2"
+	"log"
+)
+
+type LDAPAuth struct {
+	LDAPAddr string
+	DCString string
+}
+
+func (this LDAPAuth) Init() {
+
+}
+
+func (this LDAPAuth) Authenticate(Username string, Password string, InternalIP string) bool {
+	l, err := ldap.Dial("tcp", this.LDAPAddr)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer l.Close()
+
+	err = l.Bind("cn="+Username+","+this.DCString, Password)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
