e9579792d37876ee9733215aaff9f3291e3d8794
Denis Lavrov <bahus.vel@gmail.com>
2016-09-22 12:31:58 +1200

    Added config example

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ go install
 BINFILE=$(which TunnelBeast)
 ssh root@192.168.1.85 "killall TunnelBeast"
 scp $BINFILE root@192.168.1.85:/usr/local/bin/
-ssh root@192.168.1.85 "TunnelBeast"
+scp config.yml root@192.168.1.85:./
+ssh root@192.168.1.85 "TunnelBeast config.yml"
