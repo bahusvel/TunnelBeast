24b9638c18eafc3f3bbe54bedc465d8b40034368
Denis Lavrov <bahus.vel@gmail.com>
2018-01-09 16:27:36 +1300

    Initialised haskell project

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-go install
-BINFILE=$(which TunnelBeast)
-DESTINATION='192.168.1.91'
-
-ssh root@$DESTINATION "killall TunnelBeast"
-scp $BINFILE root@$DESTINATION:/usr/local/bin/
-scp config.yml root@$DESTINATION:./
-ssh root@$DESTINATION "TunnelBeast config.yml"
