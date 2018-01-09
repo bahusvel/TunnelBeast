7990b948493f38d57aa6e82e69209ad3e9168688
Denis Lavrov <bahus.vel@gmail.com>
2018-01-09 22:46:08 +1300

    Able to host index.html

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main :: IO ()
+import qualified Data.ByteString.Char8    as BS
+import           Data.Monoid
+import           Network.HTTP.Types       (status200)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+
 main = do
-  putStrLn "hello world"
+    let port = 3000
+    putStrLn $ "Listening on port " ++ show port
+    run port app
+
+app req respond = respond $ case rawPathInfo req of
+    "/" -> index
+
+yay respond = do
+    responseLBS
+        status200
+        [ ("Content-Type", "text/plain") ]
+        "yay\n"
+
+
+index :: Response
+index = responseFile
+    status200
+    [ ("Content-Type", "text/html") ]
+    "html/index.html"
+    Nothing
