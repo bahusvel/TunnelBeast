3f4b19b93e49ff739f857a2dafcc6ec816369975
steven <steven.zhangyuan@gmail.com>
2020-05-04 14:21:47 +1200

    Change authentication to lcoal DB and add user management

--- auth/auth.go
+++ auth/auth.go
@@ -1,9 +1,12 @@
 package auth
 
+import "github.com/bahusvel/TunnelBeast/boltdb"
+
 type AuthProvider interface {
 	Init()
 	CheckDestinationIP(dstip string, Username string, Password string) bool
 	Authenticate(Username string, Password string) bool
+	CheckAdminPanel(Username string, Password string) bool
 }
 
 type TestAuth struct {
@@ -19,9 +22,23 @@ func (this TestAuth) Authenticate(Username string, Password string) bool {
 	if Username == this.Username && Password == this.Password {
 		return true
 	}
+
+	key := "users/" + Username + "/" + Password
+	_, err := boltdb.GetRecord(key)
+	if err == nil {
+		return true
+	}
+
 	return false
 }
 
 func (this TestAuth) CheckDestinationIP(dstip string, Username string, Password string) bool {
 	return true
+}
+
+func (this TestAuth) CheckAdminPanel(Username string, Password string) bool {
+	if Username == this.Username && Password == this.Password {
+		return true
+	}
+	return false
 }
